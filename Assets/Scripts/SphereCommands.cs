f81eb97e0e1796b5a73a395716009740c4c684a6
Daniel Fernando Lozano <cto@fenix-alliance.com>
2018-05-12 21:29:14 -0500

    [Adds ] SphereCommands class

---
+++ Assets/Scripts/SphereCommands.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereCommands : MonoBehaviour {
+
+    // Called by GazeGestureManager when the user performs a Select gesture 
+    void OnSelect()
+    {
+        // If the sphere has no Rigidbody component, add one to enable physics.
+        if (!this.GetComponent<Rigidbody>())
+        {
+            var rigidbody = this.gameObject.AddComponent<Rigidbody>();
+            rigidbody.collisionDetectionMode = CollisionDetectionMode.Continuous;
+        }
+    }
+}
